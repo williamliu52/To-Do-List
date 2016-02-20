0dd7958708cc43218c9285630679046b834d93b8
Will Liu <williamliu52@gmail.com>
2016-02-19 23:07:42 -0500

    Create table on Completed items view

--- To Do List/TextInputTableViewCell.swift
+++ To Do List/TextInputTableViewCell.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+import UIKit
+
+public class TextInputTableViewCell: UITableViewCell {
+
+    @IBOutlet var textField: UITextField!
+    
+    public func configure(text: String?, placeholder: String) {
+        textField.text = text
+        textField.placeholder = placeholder
+        
+        textField.accessibilityValue = text
+        textField.accessibilityLabel = placeholder
+    }
+}
